abd7c1e02f380f9e951a56c2cc74a87584cc9dd4
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-09-24 12:15:27 -0600

    Changed class header names

--- Headers/descriptionEXT/GUI/bulwarkShopGUI.hpp
+++ Headers/descriptionEXT/GUI/bulwarkShopGUI.hpp
@@ -1,4 +1,4 @@
-class startBox_Dialog
+class bulwarkShopDialog
 {
     idd = 9999;
     movingEnabled = false;
@@ -7,9 +7,10 @@ class startBox_Dialog
     {
     ////////////////////////////////////////////////////////
     // GUI EDITOR OUTPUT START (by Hilltop, v1.063, #Hofuqe)
+    // Modified by: Ansible // Cipher
     ////////////////////////////////////////////////////////
 
-        class startBox_rscPicture: RscPicture
+        class bulwarkShopDialog_rscPicture: RscPicture
         {
             idc = 1200;
             text = "#(argb,8,8,3)color(0.1,0.1,0.1,0.8)";
@@ -19,7 +20,7 @@ class startBox_Dialog
             h =  0.385 * safezoneH;
         };
 
-        class startBox_buildList: RscListbox
+        class bulwarkShopDialog_buildList: RscListbox
         {
             idc = 1500;
             x = 0.31 * safezoneW + safezoneX;
@@ -31,14 +32,14 @@ class startBox_Dialog
 		class ObjectPicture: RscPicture
         {
             idc = 1502;
-			text="preview.paa";
+			text = "preview.paa";
             x = 0.1 * safezoneW + safezoneX;
             y = 0.3 * safezoneH + safezoneY;
             w = 0.2 * safezoneW;
             h = 0.2 * safezoneH;
         };
 		
-        class startBox_buildButton: RscButton
+        class bulwarkShopDialog_buildButton: RscButton
         {
             idc = 1600;
             text = "Purchase Building";
@@ -46,10 +47,10 @@ class startBox_Dialog
             y = 0.58 * safezoneH + safezoneY;
             w = 0.185 * safezoneW;
             h = 0.04 * safezoneH;
-            action = "_nil=[]ExecVM ""bulwark\purchase.sqf""";
+            action = "call BLWK_fnc_purchaseObject";
         };
 
-        class startBox_supportLst: RscListbox
+        class bulwarkShopDialog_supportLst: RscListbox
         {
             idc = 1501;
             x = 0.505 * safezoneW + safezoneX;
@@ -57,7 +58,7 @@ class startBox_Dialog
             w = 0.185 * safezoneW;
             h = 0.3 * safezoneH;
         };
-        class startBox_supportButton: RscButton
+        class bulwarkShopDialog_supportButton: RscButton
         {
             idc = 1601;
             text = "Purchase Support";
@@ -65,6 +66,7 @@ class startBox_Dialog
             y = 0.58 * safezoneH + safezoneY;
             w = 0.185 * safezoneW;
             h = 0.04 * safezoneH;
+            // CIPHER COMMENT: need to pass the new function here when done
             action = "_nil=[]ExecVM ""supports\purchase.sqf""";
         };
 
