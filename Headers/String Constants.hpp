6e1b060f81fd0cae95bc6b9ec9799c01f5a45ad4
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-10-01 19:46:05 -0600

    Added Wave Type notification strings

--- Headers/String Constants.hpp
+++ Headers/String Constants.hpp
@@ -13,3 +13,17 @@
 
 // Arrays
 #define WAVE_ENEMIES_ARRAY "BLWK_mustKillArray"
+
+// Notifications
+#define SPECIAL_WARNING_TEMPLATE "SpecialWarning"
+#define TASK_ASSIGNED_TEMPLATE "TaskAssigned"
+
+#define INCOMING_WAVE_NOTIFICATION(ROUND_NUM) ("Incoming Wave: " + #ROUND_NUM)
+#define DEFECTORS_WAVE_NOTIFICATION "Defectors Are Attacking!"
+#define DRONE_WAVE_NOTIFICATION "Enemy Drones Inbound!"
+#define OVERRUN_WAVE_NOTIFICATION "The Bulwark Was Overrun!"
+#define FOGGY_WAVE_NOTIFICATION "A Dense Fog Is Coming"
+#define NIGHT_WAVE_NOTIFICATION "The Enemy Has Blocked Out The Sun"
+#define CIVILIAN_WAVE_NOTIFICATION "Civilians Are Fleeing, Watch Your Fire!"
+#define MORTAR_WAVE_NOTIFICATION "Incoming Mortar Fire!"
+#define SUICIDE_WAVE_NOTIFICATION "Enemy Suicide Bombers Are Incoming!"
