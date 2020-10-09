239e3109bd6e9ca0b9be3d92a656507232f9401c
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-10-09 17:57:48 -0600

    Added OPTRE & OPCAN faction support

--- Headers/Faction Headers/Define Factions.hpp
+++ Headers/Faction Headers/Define Factions.hpp
@@ -12,7 +12,26 @@
 	10,\
 	11,\
 	12,\
-	13
+	13,\
+	14,\
+	15,\
+	16,\
+	17,\
+	18,\
+	19,\
+	20,\
+	21,\
+	22,\
+	23,\
+	24,\
+	25,\
+	26,\
+	27,\
+	28,\
+	29,\
+	30,\
+	31,\
+	32
 
 #define FACTION_STRINGS \
 	"NATO", \
@@ -28,7 +47,25 @@
 	"SYNDIKAT", \
 	"AAF", \
 	"LDF", \
-	"SPETSNAZ (CONTACT)"
+	"UNSC Marines (OPTRE)", \
+	"ODSTs (OPTRE)", \
+	"UNSC Army Snow (OPTRE)", \
+	"URF (OPTRE)", \
+	"Battle Jumpers Urban (OPTRE)", \
+	"Insurgents (OPTRE)", \
+	"Covenant (OPTRE-FC)", \
+	"Colonial Guard Corps (OPCAN)", \
+	"UNSC Army Desert (OPCAN)", \
+	"UNSC Army Woodland (OPCAN)", \
+	"UNSC Marine CE-A (OPCAN)", \
+	"UNSC Marine Desert (OPCAN)", \
+	"UNSC Marine Infinite (OPCAN)", \
+	"UNSC Marine Woodland (OPCAN)", \
+	"United Rebel Army (OPCAN)", \
+	"Koslovics (OPCAN)", \
+	"Fridens Woodland (OPCAN)", \
+	"Fridens Desert (OPCAN)", \
+	"Fridens(OPCAN)"
 
 #define FACTION_VARS \
 [ \
@@ -45,5 +82,24 @@
 	SYNDIKAT_UNITS, \
 	AAF_UNITS, \
 	LDF_UNITS, \
-	SPETZNAS_CONTACT_UNITS \
+	SPETZNAS_CONTACT_UNITS, \
+	OPTRE_MARINE_UNITS, \
+	OPTRE_ODST_UNITS, \
+	OPTRE_ARMY_SNOW_UNITS, \
+	OPTRE_URF_UNITS, \
+	OPTRE_BATTLEJUMPERS_URBAN_UNITS, \
+	OPTRE_INSURGENTS_UNITS, \
+	OPTRE_COVENANT_UNITS, \
+	OPCAN_CGC_UNITS, \
+	OPCAN_ARMY_D_UNITS, \
+	OPCAN_ARMY_WDL_UNITS, \
+	OPCAN_MARINE_CEA_UNITS, \
+	OPCAN_MARINE_DES_UNITS, \
+	OPCAN_MARINE_INF_UNITS, \
+	OPCAN_MARINE_WDL_UNITS, \
+	OPCAN_URA_UNITS, \
+	OPCAN_KOSLOVICS_UNITS, \
+	OPCAN_FRIDENS_WDL_UNITS, \
+	OPCAN_FRIDENS_DES_UNITS, \
+	OPCAN_FRIDENS_UNITS \
 ]
---
+++ Headers/Faction Headers/Unit Tables/optreUnitTables.hpp
@@ -0,0 +1,138 @@
+#define OPTRE_MARINE_UNITS \
+[ \
+    "OPTRE_UNSC_Marine_Soldier_AA_Specialist", \
+    "OPTRE_UNSC_Marine_Soldier_AT_Specialist", \
+    "OPTRE_UNSC_Marine_Soldier_Breacher", \
+    "OPTRE_UNSC_Marine_Soldier_Corpsman", \
+    "OPTRE_UNSC_Marine_Soldier_Demolitions", \
+    "OPTRE_UNSC_Marine_Soldier_Marksman", \
+    "OPTRE_UNSC_Marine_Soldier_Engineer", \
+    "OPTRE_UNSC_Marine_Soldier_ForwardObserver", \
+    "OPTRE_UNSC_Marine_Soldier_Grenadier", \
+    "OPTRE_UNSC_Marine_Soldier_Officer", \
+    "OPTRE_UNSC_Marine_Soldier_Radioman", \
+    "OPTRE_UNSC_Marine_Soldier_Rifleman_AT", \
+    "OPTRE_UNSC_Marine_Soldier_Rifleman_BR", \
+    "OPTRE_UNSC_Marine_Soldier_Rifleman_Light", \
+    "OPTRE_UNSC_Marine_Soldier_Rifleman_AR", \
+    "OPTRE_UNSC_Marine_Soldier_SquadLead", \
+    "OPTRE_UNSC_Marine_Soldier_TeamLead", \
+    "OPTRE_UNSC_Marine_Soldier_Autorifleman", \
+    "OPTRE_UNSC_Marine_Soldier_Assist_Autorifleman", \
+    ["OPTRE_M12_LRV","OPTRE_M12G1_LRV","OPTRE_UNSC_falcon_unarmed","OPTRE_Pelican_unarmed"]\
+]
+
+#define OPTRE_NAVY_UNITS \
+[ \
+    "OPTRE_UNSC_Navy_Soldier_Blue", \
+    "OPTRE_UNSC_Navy_Soldier_Red", \
+    "OPTRE_UNSC_Navy_Soldier_Orange", \
+    "OPTRE_UNSC_Navy_Soldier_Gray", \
+    ["","","","",""]\
+]
+
+#define OPTRE_ODST_UNITS \
+[ \
+    "OPTRE_UNSC_ODST_Soldier_Automatic_Rifleman", \
+    "OPTRE_UNSC_ODST_Soldier_DemolitionsExpert", \
+    "OPTRE_UNSC_ODST_Soldier_Marksman", \
+    "OPTRE_UNSC_ODST_Soldier_Paramedic", \
+    "OPTRE_UNSC_ODST_Soldier_Rifleman_AT", \
+    "OPTRE_UNSC_ODST_Soldier_Rifleman_BR", \
+    "OPTRE_UNSC_ODST_Soldier_Rifleman_AR", \
+    "OPTRE_UNSC_ODST_Soldier_Scout", \
+    "OPTRE_UNSC_ODST_Soldier_Scout_AT", \
+    "OPTRE_UNSC_ODST_Soldier_Scout_Sniper", \
+    "OPTRE_UNSC_ODST_Soldier_TeamLeader", \
+    ["","","","",""]\
+]
+
+#define OPTRE_ARMY_SNOW_UNITS \
+[ \
+    "OPTRE_UNSC_Army_Soldier_AA_Specialist_SNO", \
+    "OPTRE_UNSC_Army_Soldier_AT_Specialist_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Autorifleman_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Breacher_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Demolitions_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Marksman_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Engineer_SNO", \
+    "OPTRE_UNSC_Army_Soldier_ForwardObserver_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Grenadier_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Medic_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Officer_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Radioman_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Rifleman_AT_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Rifleman_BR_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Rifleman_Light_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Rifleman_AR_SNO", \
+    "OPTRE_UNSC_Army_Soldier_SquadLead_SNO", \
+    "OPTRE_UNSC_Army_Soldier_TeamLead_SNO", \
+    "OPTRE_UNSC_Army_Soldier_Assist_Autorifleman_SNO", \
+    ["","","","",""]\
+]
+
+#define OPTRE_URF_UNITS \
+[ \
+	"OPTRE_Ins_URF_AA_Specialist", \
+	"OPTRE_Ins_URF_Assist_Autorifleman", \
+	"OPTRE_Ins_URF_AT_Specialist", \
+	"OPTRE_Ins_URF_Autorifleman", \
+	"OPTRE_Ins_URF_Breacher", \
+	"OPTRE_Ins_URF_Demolitions", \
+	"OPTRE_Ins_URF_Marksman", \
+	"OPTRE_Ins_URF_Engineer", \
+	"OPTRE_Ins_URF_Grenadier", \
+	"OPTRE_Ins_URF_Medic", \
+	"OPTRE_Ins_ER_MAdvisor", \
+	"OPTRE_Ins_URF_Observer", \
+	"OPTRE_Ins_URF_Officer", \
+	"OPTRE_Ins_URF_Radioman", \
+	"OPTRE_Ins_URF_Rifleman_AT", \
+	"OPTRE_Ins_URF_Rifleman_BR", \
+	"OPTRE_Ins_URF_Rifleman_Light", \
+	"OPTRE_Ins_URF_Rifleman_AR", \
+	"OPTRE_Ins_URF_SquadLead", \
+	"OPTRE_Ins_URF_TeamLead", \
+	["OPTRE_M12_LRV_ins","LM_OPCAN_ARV_CMA_HMG_WDL","I_E_APC_tracked_03_cannon_F","LM_OPCAN_M350_INS_WDL","OPTRE_Pelican_unarmed_ins"]\
+]
+
+#define OPTRE_BATTLEJUMPERS_URBAN_UNITS \
+[ \
+	"OPTRE_Ins_BJ_Soldier_URB_Automatic_Rifleman", \
+	"OPTRE_Ins_BJ_Soldier_URB_Engineer", \
+	"OPTRE_Ins_BJ_Soldier_URB_Marksman", \
+	"OPTRE_Ins_BJ_Soldier_URB_Rifleman_AT", \
+	"OPTRE_Ins_BJ_Soldier_URB_Rifleman_BR", \
+	"OPTRE_Ins_BJ_Soldier_URB_Rifleman_AR", \
+	"OPTRE_Ins_BJ_Soldier_URB_Scout", \
+	"OPTRE_Ins_BJ_Soldier_URB_TeamLeader", \
+	"OPTRE_Ins_BJ_Soldier_URB_Corpsman", \
+	["","","","",""]\ 
+]
+
+#define OPTRE_INSURGENTS_UNITS \
+[ \
+	"OPTRE_Ins_ER_Farmer", \
+	"OPTRE_Ins_ER_Guerilla_AR", \
+	"OPTRE_Ins_ER_Hacker", \
+	"OPTRE_Ins_ER_Insurgent_BR", \
+	"OPTRE_Ins_ER_Militia_MG", \
+	"OPTRE_Ins_ER_Rebel_AT", \
+	"OPTRE_Ins_ER_Surgeon", \
+	"OPTRE_Ins_ER_Terrorist", \
+	"OPTRE_Ins_ER_Warlord", \
+	"OPTRE_Ins_ER_Deserter_GL", \
+	["OPTRE_M12_LRV_ins","LM_OPCAN_ARV_CMA_HMG_WDL","I_E_APC_tracked_03_cannon_F","LM_OPCAN_M350_INS_WDL","OPTRE_Pelican_unarmed_ins"]\
+]
+
+#define OPTRE_COVENANT_UNITS \
+[ \
+	"OPTRE_FC_Elite_FieldMarshal", \
+	"OPTRE_FC_Elite_HonorGuard", \
+	"OPTRE_FC_Elite_Major", \
+	"OPTRE_FC_Elite_Minor", \
+	"OPTRE_FC_Elite_SpecOps", \
+	"OPTRE_FC_Elite_Ultra", \
+	"OPTRE_FC_Elite_Zealot", \
+	["","","","",""]\
+]
