2e5905eff7769bf44cd40b137d28cbaf5e97908c
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-10-10 13:08:42 -0600

    Testing new info panel

--- Functions/Init Functions/fn_infoPanelLoop.sqf
+++ Functions/Init Functions/fn_infoPanelLoop.sqf
@@ -27,9 +27,76 @@ Author:
 // This may be replaced in the future with an update function that simply acts as an event that must be called
 // I am curious to see if the performance of this loop is negligible enough though, 
 /// that it would be more effective to just have these update automatically
-
 if (!hasInterface OR {!canSuspend}) exitWith {};
 
+disableSerialization;
+
+#include "..\..\Headers\descriptionEXT\GUI\infoPanelCommonDefines.hpp"
+
+waitUntil {
+	if ((!isNil "BLWK_playerKillPoints") AND {!isNil "BLWK_numRespawnTickets"} AND {!isNil "BLWK_currentWaveNumber"}) exitWith {true};
+	sleep 1;
+	false
+};
+
+
+
+private "_infoPanelDisplay";
+waitUntil {
+	_infoPanelDisplay = findDisplay INFO_PANEL_IDD;
+	if (!isNull _infoPanelDisplay) exitWith {true};
+	false
+};
+
+// player points
+private _playerPoints = BLWK_playerKillPoints;
+private _pointsCtrl = _infoPanelDisplay displayCtrl INFO_PANEL_PLAYER_POINTS_IDC;
+private _fn_updatePlayerPoints = {	
+	_pointsCtrl ctrlSetText (str BLWK_playerKillPoints);
+	_playerPoints = BLWK_playerKillPoints;
+};
+
+// respawn tickets
+private _numRespawnTickets = BLWK_numRespawnTickets;
+private _ticketsCtrl = _infoPanelDisplay displayCtrl INFO_PANEL_RESPAWNS_NUM_IDC;
+private _fn_updateRespawnTickets = {
+	_ticketsCtrl ctrlSetText (str BLWK_numRespawnTickets);
+	_numRespawnTickets = BLWK_numRespawnTickets;
+};
+
+// current wave number
+private _currentWave = BLWK_currentWaveNumber;
+private _currentWaveCtrl = _infoPanelDisplay displayCtrl INFO_PANEL_WAVE_NUM_IDC;
+private _fn_updateCurrentWave = {
+	_currentWaveCtrl ctrlSetText (str BLWK_currentWaveNumber);
+	_currentWave = BLWK_currentWaveNumber;
+};
+
+// in between waves notice
+private _inBetweenWaves = BLWK_inBetweenWaves;
+private _inbetweenWavesCtrl = _infoPanelDisplay displayCtrl INFO_PANEL_WAVE_STATUS_IDC;
+private _fn_updateInbetweenWaves = {
+	if (BLWK_inBetweenWaves) then {
+		_inbetweenWavesCtrl ctrlSetText "IN BETWEEN";
+		_inbetweenWavesCtrl ctrlSetTextColor [0,0.75,0.22,0.7];
+	} else {
+		_inbetweenWavesCtrl ctrlSetText "IN PROGRESS";
+		_inbetweenWavesCtrl ctrlSetTextColor [1,0.1,0,0.57];
+	};
+	_inBetweenWaves = BLWK_inBetweenWaves;
+};
+
+1000 cutRsc ["BLWK_infoPanel","PLAIN"];
+
+while {sleep 2; true} do {
+	if (_playerPoints != BLWK_playerKillPoints) then {call _fn_updatePlayerPoints};
+	if (_numRespawnTickets != BLWK_numRespawnTickets) then {call _fn_updateRespawnTickets};
+	if (_currentWave != BLWK_currentWaveNumber) then {call _fn_updateCurrentWave};
+	if (_inBetweenWaves != BLWK_inBetweenWaves) then {call _fn_updateInbetweenWaves};
+};
+
+
+/*
 // CIPHER COMMENT: it would be worth removing the player name in the future, not much point in having it
 params [
 	["_player",player,[objNull]]
@@ -52,6 +119,7 @@ private _currentWave = BLWK_currentWaveNumber;
 private "_text";
 
 private _fn_updateInfoPanel = {
+	hint "panel updated";
 	_playerPoints = BLWK_playerKillPoints;
 	_numRespawnTickets = BLWK_numRespawnTickets;
 	_currentWave = BLWK_currentWaveNumber;
@@ -92,3 +160,4 @@ while {sleep 2; true} do {
 		call _fn_updateInfoPanel
 	};
 };
+*/
--- Headers/descriptionEXT/GUI/infoPanel.hpp
+++ Headers/descriptionEXT/GUI/infoPanel.hpp
@@ -1,3 +1,4 @@
+/*
 #define INFO_PANEL_IDC 99999
 
 class BLWK_infoPanel {
@@ -28,6 +29,114 @@ class BLWK_infoPanel {
 			class Attributes {
 				align = "left";
 			};
+		};
+	};
+};
+*/
+
+#include "infoPanelCommonDefines.hpp"
+class BLWK_infoPanel {
+	idd = INFO_PANEL_IDD;
+	fadeout = 0;
+    fadein = 0;
+	duration = 1e+011; // we want this to be up for as long as the mission is
+	name= "BLWK_infoPanel";
+
+	class controlsBackground {
+		class BLWK_infoPanel_frame_static: RscFrame
+		{
+			idc = -1;
+			x = 0.00781247 * safezoneW + safezoneX;
+			y = 0.0104166 * safezoneH + safezoneY;
+			w = 0.0996094 * safezoneW;
+			h = 0.145833 * safezoneH;
+			colorBackground[] = {0,0,0,0.3};
+			shadow = 2;
+		};
+		class BLWK_infoPanel_playerName_static: RscText
+		{
+			idc = -1;
+			text = ""; //--- ToDo: Localize;
+			x = 0.0136719 * safezoneW + safezoneX;
+			y = 0.0208333 * safezoneH + safezoneY;
+			w = 0.0878906 * safezoneW;
+			h = 0.03125 * safezoneH;
+			onLoad = "(_this select 0) ctrlSetText (name player)"
+			colorBackground[] = {-1,-1,-1,-1};
+		};
+		class BLWK_infoPanel_playerPoints_shower: RscText
+		{
+			idc = INFO_PANEL_PLAYER_POINTS_IDC;
+			text = ""; //--- ToDo: Localize;
+			x = 0.0136718 * safezoneW + safezoneX;
+			y = 0.0520833 * safezoneH + safezoneY;
+			w = 0.0878906 * safezoneW;
+			h = 0.03125 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+		};
+		class BLWK_infoPanel_wave_static: RscText
+		{
+			idc = -1;
+			text = "Wave:"; //--- ToDo: Localize;
+			x = 0.0136718 * safezoneW + safezoneX;
+			y = 0.104167 * safezoneH + safezoneY;
+			w = 0.0644531 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+			sizeEx = 0.75 * UI_GRID_H;
+		};
+		class BLWK_infoPanel_wave_number: RscText
+		{
+			idc = INFO_PANEL_WAVE_NUM_IDC;
+			text = ""; //--- ToDo: Localize;
+			x = 0.078125 * safezoneW + safezoneX;
+			y = 0.104167 * safezoneH + safezoneY;
+			w = 0.0234375 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+		};
+		class BLWK_infoPanel_respawnsLeft_number: RscText
+		{
+			idc = INFO_PANEL_RESPAWNS_NUM_IDC;
+			text = ""; //--- ToDo: Localize;
+			x = 0.078125 * safezoneW + safezoneX;
+			y = 0.125 * safezoneH + safezoneY;
+			w = 0.0234375 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+		};
+		class BLWK_infoPanel_respawnsLeft_static: RscText
+		{
+			idc = -1;
+			text = "Respawns Left:"; //--- ToDo: Localize;
+			x = 0.0136718 * safezoneW + safezoneX;
+			y = 0.125 * safezoneH + safezoneY;
+			w = 0.0644531 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+			sizeEx = 0.75 * UI_GRID_H;
+		};
+		class BLWK_infoPanel_waveStatus_static: RscText
+		{
+			idc = -1;
+			text = "Wave Status:"; //--- ToDo: Localize;
+			x = 0.0136718 * safezoneW + safezoneX;
+			y = 0.0833333 * safezoneH + safezoneY;
+			w = 0.0410156 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+			sizeEx = 0.75 * UI_GRID_H;
+		};
+		class BLWK_infoPanel_waveStatus_shower: RscText
+		{
+			idc = INFO_PANEL_WAVE_STATUS_IDC;
+			text = ""; //--- ToDo: Localize;
+			x = 0.0546875 * safezoneW + safezoneX;
+			y = 0.0833333 * safezoneH + safezoneY;
+			w = 0.046875 * safezoneW;
+			h = 0.0208333 * safezoneH;
+			colorBackground[] = {-1,-1,-1,-1};
+			sizeEx = 0.75 * UI_GRID_H;
 		};
 	};
 };
