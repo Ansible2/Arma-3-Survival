932b2bd38071bb67b66b32ad8a95eb77b5ab7944
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-10-24 14:27:07 -0600

    Started Work on #106

---
+++ Headers/Faction Headers/Unit Tables/RHS_AFRF Unit Tables.hpp
@@ -0,0 +1,86 @@
+// MSV
+#define RHS_AFRF_RUS_MSV_EMR_UNITS \
+[\
+	"rhs_msv_emr_at",\
+	"rhs_msv_emr_arifleman",\
+	"rhs_msv_emr_engineer",\
+	"rhs_msv_emr_grenadier_rpg",\
+	"rhs_msv_emr_strelok_rpg_assist",\
+	"rhs_msv_emr_junior_sergeant",\
+	"rhs_msv_emr_machinegunner",\
+	"rhs_msv_emr_machinegunner_assistant",\
+	"rhs_msv_emr_marksman",\
+	"rhs_msv_emr_medic",\
+	"rhs_msv_emr_rifleman",\
+	"rhs_msv_emr_grenadier",\
+	"rhs_msv_emr_LAT",\
+	"rhs_msv_emr_RShG2",\
+	"rhs_msv_emr_sergeant"\
+	["rhs_tigr_sts_msv","rhs_btr70_msv","rhs_bmp3_msv","rhs_t80um","rhs_ka60_c","RHS_Mi8mt_vvsc","RHS_Su25SM_vvsc"]\
+]
+#define RHS_AFRF_RUS_MSV_FLORA_UNITS \
+[\
+	"rhs_msv_at",\
+	"rhs_msv_arifleman",\
+	"rhs_msv_engineer",\
+	"rhs_msv_grenadier_rpg",\
+	"rhs_msv_strelok_rpg_assist",\
+	"rhs_msv_junior_sergeant",\
+	"rhs_msv_machinegunner",\
+	"rhs_msv_machinegunner_assistant",\
+	"rhs_msv_marksman",\
+	"rhs_msv_medic",\
+	"rhs_msv_rifleman",\
+	"rhs_msv_grenadier",\
+	"rhs_msv_LAT",\
+	"rhs_msv_RShG2",\
+	"rhs_msv_sergeant"\
+	["rhs_tigr_sts_msv","rhs_btr70_msv","rhs_bmp3_msv","rhs_t80um","rhs_ka60_c","RHS_Mi8mt_vvsc","RHS_Su25SM_vvsc"]\
+]
+
+
+// VDV
+#define RHS_AFRF_RUS_VDV_EMR_UNITS \
+[\
+	"rhs_vdv_at",\
+	"rhs_vdv_arifleman",\
+	"rhs_vdv_engineer",\
+	"rhs_vdv_grenadier_rpg",\
+	"rhs_vdv_strelok_rpg_assist",\
+	"rhs_vdv_junior_sergeant",\
+	"rhs_vdv_machinegunner",\
+	"rhs_vdv_machinegunner_assistant",\
+	"rhs_vdv_marksman",\
+	"rhs_vdv_marksman_asval",\
+	"rhs_vdv_medic",\
+	"rhs_vdv_rifleman",\
+	"rhs_vdv_rifleman_asval",\
+	"rhs_vdv_grenadier",\
+	"rhs_vdv_grenadier_alt",\
+	"rhs_vdv_LAT",\
+	"rhs_vdv_RShG2",\
+	"rhs_vdv_rifleman_alt",\
+	"rhs_vdv_sergeant"\
+	["rhs_tigr_sts_vdv","rhs_btr80a_vdv","rhs_bmd4_vdv","rhs_sprut_vdv","rhs_ka60_c","RHS_Mi8mt_vdv","RHS_Su25SM_vvsc"]\
+]
+#define RHS_AFRF_RUS_VDV_EMR_DES_UNITS \
+[\
+	"rhs_vdv_des_at",\
+	"rhs_vdv_des_arifleman",\
+	"rhs_vdv_des_engineer",\
+	"rhs_vdv_des_grenadier_rpg",\
+	"rhs_vdv_des_strelok_rpg_assist",\
+	"rhs_vdv_des_junior_sergeant",\
+	"rhs_vdv_des_machinegunner",\
+	"rhs_vdv_des_machinegunner_assistant",\
+	"rhs_vdv_des_marksman",\
+	"rhs_vdv_des_marksman_asval",\
+	"rhs_vdv_des_medic",\
+	"rhs_vdv_des_rifleman",\
+	"rhs_vdv_des_rifleman_asval",\
+	"rhs_vdv_des_grenadier",\
+	"rhs_vdv_des_LAT",\
+	"rhs_vdv_des_RShG2",\
+	"rhs_vdv_des_sergeant"\
+	["rhs_tigr_sts_3camo_vdv","rhs_btr80a_vdv","rhs_bmd4_vdv","rhs_sprut_vdv","rhs_ka60_c","RHS_Mi8mt_vdv","RHS_Su25SM_vvsc"]\
+]
