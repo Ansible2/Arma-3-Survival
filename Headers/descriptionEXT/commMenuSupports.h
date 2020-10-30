1dc41ae814a8131d258dccf85be29504a95bd75b
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-10-29 19:47:45 -0600

    Got rid of excess comments from commMenuSupports.h

--- Headers/descriptionEXT/commMenuSupports.h
+++ Headers/descriptionEXT/commMenuSupports.h
@@ -12,6 +12,11 @@
 #define SUPPLY_DROP_ICON "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\supplydrop_ca.paa"
 #define TRANSPORT_ICON "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\transport_ca.paa"
 
+/*
+    This master function for supports is used as go between for error cases such as when 
+     a player provides an invalid position (looking at the sky). It will then refund the
+     support back to the player.
+*/
 #define CALL_SUPPORT_MASTER(CLASS) "[_this select 0,_this select 1,"#CLASS"] call BLWK_fnc_callingForSupportMaster"
 
 /*
@@ -152,7 +157,7 @@ class CAS_GUNS_AND_ROCKETS_CLASS : basicSupport
 };
 
 
-// Turrets 
+// Aircraft gunner turrets 
 class TURRET_DOOR_GUNNER_CLASS : basicSupport
 {
     text = TURRET_DOOR_GUNNER_TEXT;
@@ -171,105 +176,3 @@ class TURRET_GUNSHIP_CLASS : basicSupport
     expression = CALL_SUPPORT_MASTER(TURRET_GUNSHIP_CLASS);
     icon = CAS_ICON;
 };
-
-
-/*
-class paraDrop
-{
-    text = "Paratroops";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'paraTroop', 'B_T_VTOL_01_vehicle_F'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\attack_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class reconUAV
-{
-    text = "Recon UAV";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'reconUAV', 'B_UAV_01_F'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\defend_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class airStrike
-{
-    text = "Missle CAS";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'airStrike', 'B_Plane_CAS_01_DynamicLoadout_F'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\cas_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class ragePack
-{
-    text = "Rage Stimpack";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'ragePack'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\transport_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class armaKart
-{
-    text = "ARMAKART TM";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'armaKart'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\transport_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class mindConGas
-{
-    text = "Mind Control Gas";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'mindConGas'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\call_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class droneControl
-{
-    text = "Predator Drone";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'droneControl'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\cas_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class mineField
-{
-    text = "Mine Field";
-    submenu = "";
-    expression = "[_this select 0, _this select 1, 'mineField'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\Artillery_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-
-class telePlode
-{
-    text = "Emergency Teleport";
-    submenu = "";
-    expression = "[_this select 0, _this select 1,'telePlode'] remoteExec ['killPoints_fnc_support', 2];";
-    icon = "\a3\Ui_f\data\GUI\Cfg\CommunicationMenu\transport_ca.paa";
-    cursor = "\a3\Ui_f\data\IGUI\Cfg\Cursors\iconCursorSupport_ca.paa";
-    enable = "1";
-    removeAfterExpressionCall = 1;
-};
-*/
