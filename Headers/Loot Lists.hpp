e4e7a891878fd0136568e759f8e26db6b944363d
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-11-12 08:12:33 -0700

    Fix #193 : Blacklist RHS blank mags

--- Headers/Loot Lists.hpp
+++ Headers/Loot Lists.hpp
@@ -28,7 +28,14 @@
     "8Rnd_82mm_Mo_Flare_white", \
     "muzzle_antenna_01_f", \
     "V_Safety_yellow_F", \
-    "muzzle_antenna_02_f" \
+    "muzzle_antenna_02_f", \
+    "rhsusf_100Rnd_556x45_M200_soft_pouch_ucp", \
+    "rhsusf_100Rnd_556x45_M200_soft_pouch_coyote", \
+    "rhsusf_100Rnd_556x45_M200_soft_pouch", \
+    "rhs_mag_30Rnd_556x45_M200_Stanag", \
+    "rhs_mag_20Rnd_556x45_M200_Stanag", \
+    "rhsusf_100Rnd_762x51_m82_blank", \
+    "rhsusf_50Rnd_762x51_m82_blank" \
 ]
 
 
