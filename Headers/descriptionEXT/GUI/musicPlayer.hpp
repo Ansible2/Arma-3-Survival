7d99ed0715e0ecb41b1378426d9aba2e32a897db
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-11-24 00:13:58 -0700

    Update musicPlayer.hpp

--- Headers/descriptionEXT/GUI/musicPlayer.hpp
+++ Headers/descriptionEXT/GUI/musicPlayer.hpp
@@ -483,27 +483,42 @@ BLWK_fnc_musicManagerOnload_systemOnOffCombo = {
 
 	private _systemOn = ["KISKA_musicSystemIsRunning",missionNamespace,2,false] call KISKA_fnc_getVariableTarget;
 	_control lbAdd "SYSTEM IS: OFF"; // 0
+	_control lbSetTooltip [0,"Setting the system directly to off while music is playing will have that song finish"];
+	
 	_control lbAdd "SYSTEM IS: ON"; // 1
+	_control lbSetTooltip [1,"Committed playlist on server will immediately begin playing"];
+	
+	_control lbAdd "DO SYSTEM RESET"; // 2
+	_control lbSetTooltip [2,"Current playing track from the playlist will cease on all machines and system will be set to off"];
+
 	_control ctrlSetFont "PuristaLight";
 	_control lbSetCurSel ([0,1] select _systemOn);
 
-	_control ctrlAddEventHandler ["onLBSelChanged",{
+	_control ctrlAddEventHandler ["LBSelChanged",{
 		params ["_control", "_selectedIndex"];
 		
 		private _display = ctrlParent _control;
-		if (_selectedIndex isEqualTo 0) then {
-			missionNamespace setVariable ["KISKA_musicSystemIsRunning",false,2];
-			hint "system off";
-			
-			null = remoteExec ["KISKA_fnc_stopMusic",0];
-		} else {
-			missionNamespace setVariable ["KISKA_musicSystemIsRunning",true,2];
-			hint "system on";
-
-
+		switch (_selectedIndex) do {
+			case 0:{ // system off
+				//missionNamespace setVariable ["KISKA_musicSystemIsRunning",false,2];
+				if (missionNamespace getVariable ["BLWK_musicManager_reset",false]) then {
+					hint "System reseting...";
+					missionNamespace setVariable ["BLWK_musicManager_reset",false];
+				} else {
+					hint "System OFF, last played song will finish";
+				};
+			};
+			case 1:{ // system on
+				hint "System starting...";
+				//null = [] remoteExec ["KISKA_fnc_randomMusic",2];
+			};
+			case 2:{ // system reset
+				//null = remoteExecCall ["KISKA_fnc_stopRandomMusicServer",2];
+				missionNamespace setVariable ["BLWK_musicManager_reset",true];
+				_control lbSetCurSel 0; // set to appear off
+			};
 		};
 	}];
-
 };
 
 
@@ -513,10 +528,8 @@ BLWK_fnc_musicManagerOnLoad = {
 	// available tracks list
 	[_display displayCtrl BLWK_MUSIC_MANAGER_SONGS_LIST_IDC] call BLWK_fnc_musicManagerOnload_availableMusicList;
 	
-
-
 	// system on off combo
-	null = [_display,_display displayCtrl BLWK_MUSIC_MANAGER_ONOFF_COMBO_IDC] spawn BLWK_fnc_musicManagerOnload_systemOnOffCombo;
+	null = [_display displayCtrl BLWK_MUSIC_MANAGER_ONOFF_COMBO_IDC,_display] spawn BLWK_fnc_musicManagerOnload_systemOnOffCombo;
 
 
 	// volume slider
