d381aeea1714d672d841c020d5d349ac0a459aa0
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-12-13 11:38:21 -0700

    More work on #298 for Project OPFOR

--- Headers/descriptionEXT/Faction Headers/Project OPFOR Unit Table.hpp
+++ Headers/descriptionEXT/Faction Headers/Project OPFOR Unit Table.hpp
@@ -171,3 +171,232 @@ class PROJOP_ISIS_faction : RHSGREF_CHDKZ
 		"LOP_ISTS_OPF_Infantry_SL"
 	};
 };
+
+class PROJOP_KPA_faction : RHSGREF_CHDKZ
+{
+	displayName = "PROJECT OPFOR - Korean Peoples Army";
+
+
+	lightCars[] = {
+		"LOP_NK_UAZ_AGS",
+		"LOP_NK_UAZ_DshKM"
+	};
+	heavyCars[] = {
+		"rhsgref_BRDM2"
+	};
+	lightArmor[] = {
+		"LOP_NK_BTR60",
+		"LOP_NK_BTR80",
+		"LOP_NK_BMP1"
+	};
+	heavyArmor[] = {
+		"LOP_NK_T34",
+		"LOP_NK_T55",
+		"LOP_NK_T72BA",
+		"LOP_NK_T72BB"
+	};
+	infantry[] = {
+		"LOP_NK_Infantry_AT_Asst",
+		"LOP_NK_Infantry_AT",
+		"LOP_NK_Infantry_Corpsman",
+		"LOP_NK_Infantry_Engineer",
+		"LOP_NK_Infantry_Grenadier",
+		"LOP_NK_Infantry_MG",
+		"LOP_NK_Infantry_MG_Asst",
+		"LOP_NK_Infantry_Marksman",
+		"LOP_NK_Infantry_Rifleman",
+		"LOP_NK_Infantry_Rifleman_2",
+		"LOP_NK_Infantry_SL",
+		"LOP_NK_Infantry_TL"
+	};
+};
+
+class PROJOP_MEM_faction : PROJOP_KPA_faction
+{
+	displayName = "PROJECT OPFOR - Middle East Militia";
+
+	lightCars[] = {
+		"LOP_AM_OPF_Landrover_M2",
+		"LOP_AM_OPF_Nissan_PKM",
+		"LOP_AM_OPF_UAZ_AGS",
+		"LOP_AM_OPF_UAZ_DshKM"
+	};
+	lightArmor[] = {
+		"LOP_AM_OPF_BTR60"
+	};
+	infantry[] = {
+		"LOP_AM_OPF_Infantry_Engineer",\
+		"LOP_AM_OPF_Infantry_Corpsman",\
+		"LOP_AM_OPF_Infantry_GL",\
+		"LOP_AM_OPF_Infantry_Rifleman_6",\
+		"LOP_AM_OPF_Infantry_Rifleman",\
+		"LOP_AM_OPF_Infantry_Rifleman_2",\
+		"LOP_AM_OPF_Infantry_Rifleman_4",\
+		"LOP_AM_OPF_Infantry_Rifleman_5",\
+		"LOP_AM_OPF_Infantry_Rifleman_7",\
+		"LOP_AM_OPF_Infantry_Rifleman_8",\
+		"LOP_AM_OPF_Infantry_AT",\
+		"LOP_AM_OPF_Infantry_Marksman",\
+		"LOP_AM_OPF_Infantry_Rifleman_9",\
+		"LOP_AM_OPF_Infantry_AR",\
+		"LOP_AM_OPF_Infantry_AR_Asst",\
+		"LOP_AM_OPF_Infantry_SL"\
+	};
+};
+
+class PROJOP_SLA_faction : RHSGREF_CDF_faction
+{
+	displayName = "PROJECT OPFOR - Sahrani Liberation Army";
+
+	lightCars[] = {
+		"LOP_SLA_UAZ_AGS",
+		"LOP_SLA_UAZ_DshKM"		
+	};
+	heavyCars[] = {
+		"rhsgref_BRDM2_ins"
+	};
+	lightArmor[] = {
+		"LOP_SLA_BTR60",
+		"LOP_SLA_BTR70",
+		"LOP_SLA_BMP1",
+		"LOP_SLA_BMP1D",
+		"LOP_SLA_BMP2",
+		"LOP_SLA_BMP2D"
+	};
+	heavyArmor[] = {
+		"LOP_SLA_T72BA",
+		"LOP_SLA_T72BB"
+	};
+	transportHelicopters[] = {
+		"LOP_SLA_Mi8MTV3_UPK23"
+	};
+	infantry[] = {
+		"LOP_SLA_Infantry_AA",\
+		"LOP_SLA_Infantry_AT_Asst",\
+		"LOP_SLA_Infantry_Corpsman",\
+		"LOP_SLA_Infantry_Engineer",\
+		"LOP_SLA_Infantry_GL",\
+		"LOP_SLA_Infantry_AT",\
+		"LOP_SLA_Infantry_MG",\
+		"LOP_SLA_Infantry_MG_Asst",\
+		"LOP_SLA_Infantry_Marksman",\
+		"LOP_SLA_Infantry_Rifleman",\
+		"LOP_SLA_Infantry_Rifleman_2",\
+		"LOP_SLA_Infantry_SL",\
+		"LOP_SLA_Infantry_TL"\
+	};
+};
+
+class PROJOP_SAF_faction : PROJOP_SLA_faction
+{
+	displayName = "PROJECT OPFOR - Syrian Armed Forces";
+
+	heavyArmor[] = {
+		"LOP_SYR_T55",
+		"LOP_SYR_T72BA",
+		"LOP_SYR_T72BB"
+	};
+	infantry[] = {
+		"LOP_SYR_Infantry_AT_Asst",\
+		"LOP_SYR_Infantry_AT",\
+		"LOP_SYR_Infantry_Corpsman",\
+		"LOP_SYR_Infantry_Engineer",\
+		"LOP_SYR_Infantry_Grenadier",\
+		"LOP_SYR_Infantry_MG",\
+		"LOP_SYR_Infantry_MG_Asst",\
+		"LOP_SYR_Infantry_Marksman",\
+		"LOP_SYR_Infantry_Rifleman",\
+		"LOP_SYR_Infantry_Rifleman_2",\
+		"LOP_SYR_Infantry_SL",\
+		"LOP_SYR_Infantry_TL"\		
+	};
+};
+
+class PROJOP_TAF_faction : PROJOP_SLA_faction
+{
+	displayName = "PROJECT OPFOR - Takistan Armed Forces";
+
+	lightCars[] = {
+		"LOP_TKA_UAZ_AGS",
+		"LOP_TKA_UAZ_DshKM"
+	};
+	lightArmor[] = {
+		"LOP_TKA_BTR60",
+		"LOP_TKA_BTR70",
+		"LOP_TKA_BMP1",
+		"LOP_TKA_BMP1D",
+		"LOP_TKA_BMP2",
+		"LOP_TKA_BMP2D"
+	};
+	heavyArmor[] = {
+		"LOP_TKA_T34",
+		"LOP_TKA_T55",
+		"LOP_TKA_T72BA",
+		"LOP_TKA_T72BB"
+	};
+	transportHelicopters[] = {
+		"LOP_TKA_Mi8MTV3_UPK23"
+	};
+
+	infantry[] = {
+		"LOP_TKA_Infantry_AA",\
+		"LOP_TKA_Infantry_Corpsman",\
+		"LOP_TKA_Infantry_Engineer",\
+		"LOP_TKA_Infantry_GL",\
+		"LOP_TKA_Infantry_AT",\
+		"LOP_TKA_Infantry_AT_Asst",\
+		"LOP_TKA_Infantry_MG",\
+		"LOP_TKA_Infantry_MG_Asst",\
+		"LOP_TKA_Infantry_Marksman",\
+		"LOP_TKA_Infantry_Rifleman",\
+		"LOP_TKA_Infantry_Rifleman_2",\
+		"LOP_TKA_Infantry_Rifleman_3",\
+		"LOP_TKA_Infantry_SL",\
+		"LOP_TKA_Infantry_TL"\
+	};
+};
+
+class PROJOP_NAF_faction : PROJOP_TAF_faction
+{
+	displayName = "PROJECT OPFOR - Novorossiya Armed Forces";
+
+	lightCars[] = {
+		"LOP_US_UAZ_AGS",
+		"LOP_US_UAZ_DshKM"
+	};
+	lightArmor[] = {
+		"LOP_US_BTR60",
+		"LOP_US_BTR70",
+		"LOP_US_BMP1",
+		"LOP_US_BMP1D",
+		"LOP_US_BMP2",
+		"LOP_US_BMP2D"
+	};
+	heavyArmor[] = {
+		"LOP_US_T72BA",
+		"LOP_US_T72BB",
+		"LOP_US_T72BC"
+	};
+	infantry[] = {
+		"LOP_US_Infantry_AA",\
+		"LOP_US_Infantry_Engineer",\
+		"LOP_US_Infantry_GL",\
+		"LOP_US_Infantry_GL_2",\
+		"LOP_US_Infantry_AT",\
+		"LOP_US_Infantry_AT_Asst",\
+		"LOP_US_Infantry_SL",\
+		"LOP_US_Infantry_MG_3",\
+		"LOP_US_Infantry_MG",\
+		"LOP_US_Infantry_MG_2",\
+		"LOP_US_Infantry_MG_Asst",\
+		"LOP_US_Infantry_Marksman",\
+		"LOP_US_Infantry_Corpsman",\
+		"LOP_US_Infantry_Officer",\
+		"LOP_US_Infantry_Rifleman",\
+		"LOP_US_Infantry_Rifleman_2",\
+		"LOP_US_Infantry_Rifleman_3",\
+		"LOP_US_Infantry_Rifleman_4",\
+		"LOP_US_Infantry_TL"\
+	};
+};
+
