2e1f81b4947b3f6162859b094a8e63ae99f30d79
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-12-19 14:45:04 -0700

    Removed old faction tables

--- Headers/Faction Headers/Unit Tables/Master Unit Table.hpp
+++
@@ -1,8 +0,0 @@
-#include "vanillaUnitTables.hpp"
-#include "optreUnitTables.hpp"
-#include "opcanUnitTables.hpp"
-#include "RHS_AFRF Unit Tables.hpp"
-#include "RHS_USAF Unit Tables.hpp"
-#include "Z&DUnitTables.hpp"
-#include "RHS_GREF Unit Tables.hpp"
-#include "Project OPFOR Unit Tables.hpp"
