ceb009389c1a4ec1d63d97f48dcdd464a9d053c6
Ansible2 <afoshark@yahoo.com>
2021-08-16 22:59:56 -0600

    Fix #556 & Fix #555 : Blacklist OPTRE spartan & elite gear

--- Headers/descriptionEXT/Loot Lists/OPTRE Loot Lists.hpp
+++ Headers/descriptionEXT/Loot Lists/OPTRE Loot Lists.hpp
@@ -1,3 +1,5 @@
+#include "Supplements\Spartan Gear.hpp"
+
 #define OJOO_CONDITION "private _name = toUpperANSI (_this select 0); if ('OPTRE_' in _name OR {'JTFS_' in _name} OR {'OPAEX_' in _name} OR {'LM_OPCAN' in _name}) then {true} else {false}"
 #define OPTRE_CONDITION "'OPTRE_' in (toUpperANSI (_this select 0))"
 #define OPTRE_OPAEX_CONDITION "private _name = toUpperANSI (_this select 0); if ('OPTRE_' in _name OR {'OPAEX_' in _name}) then {true} else {false}"
@@ -14,11 +16,32 @@ class OPTRE_lootList
     conditionClothes = OPTRE_CONDITION;
     conditionMagazines = OPTRE_CONDITION;
 
+    lootBlackList[] = {
+        "OPTRE_FC_Elite_Armor_FieldMarshal",
+        "OPTRE_FC_Elite_Armor_HonorGuard",
+        "OPTRE_FC_Elite_Armor_HonorGuard_Ultra",
+        "OPTRE_FC_Elite_Armor_Major",
+        "OPTRE_FC_Elite_Armor_Minor",
+        "OPTRE_FC_Elite_Armor_SpecOps",
+        "OPTRE_FC_Elite_Armor_Ultra",
+        "OPTRE_FC_Elite_Armor_Zealot",
+        "OPTRE_FC_Elite_CombatSkin",
+        "OPTRE_FC_Elite_Helmet_FieldMarshal",
+        "OPTRE_FC_Elite_Helmet_HonorGuard",
+        "OPTRE_FC_Elite_Helmet_HonorGuard_Ultra",
+        "OPTRE_FC_Elite_Helmet_Major",
+        "OPTRE_FC_Elite_Helmet_Minor",
+        "OPTRE_FC_Elite_Helmet_SpecOps",
+        "OPTRE_FC_Elite_Helmet_Ultra",
+        "OPTRE_FC_Elite_Helmet_Zealot",
+        SPARTAN_GEAR
+    };
+
     checkForDuplicates = 0;
 };
 
 
-class OPTRE_OPAEX_lootList
+class OPTRE_OPAEX_lootList : OPTRE_lootList
 {
     title = "OPTRE and OPAEX";
     patches[] = {"OPTRE_Weapons","OPAEX_Core"};
@@ -31,7 +54,7 @@ class OPTRE_OPAEX_lootList
 };
 
 
-class OPTRE_OPCAN_lootList
+class OPTRE_OPCAN_lootList : OPTRE_lootList
 {
     title = "OPTRE and OPCAN";
     patches[] = {"OPTRE_Weapons","LM_OPCAN_UNSC"};
@@ -44,7 +67,7 @@ class OPTRE_OPCAN_lootList
 };
 
 
-class OPTRE_OPCAN_OPAEX_lootList
+class OPTRE_OPCAN_OPAEX_lootList : OPTRE_lootList
 {
     title = "OPTRE, OPCAN, and OPAEX";
     patches[] = {"OPTRE_Weapons","OPAEX_Core","LM_OPCAN_UNSC"};
@@ -57,7 +80,7 @@ class OPTRE_OPCAN_OPAEX_lootList
 };
 
 
-class optre_jtfs_opaex_opcan_lootList
+class optre_jtfs_opaex_opcan_lootList : OPTRE_lootList
 {
     title = "OPTRE, JTFS, OPCAN, and OPAEX Loot List";
 
@@ -68,9 +91,6 @@ class optre_jtfs_opaex_opcan_lootList
     patches[] = {"OPTRE_Weapons","OPAEX_Core","LM_OPCAN_UNSC","JTFS_URF_Units"};
 
     checkForDuplicates = 0;
-
-    lootBlackList[] = {
-    };
 
     lootWhitelist_launchers[] = {
     };
