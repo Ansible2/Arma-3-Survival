2bfee7ebe75f9dfa7ca3f8e0e68f9d3bcb767059
Ansible2 <afoshark@yahoo.com>
2021-08-20 12:24:15 -0600

    Allowed clients to view mission parameter menu

--- KISKA Systems/KISKA Parameter Menu/Headers/params menu.hpp
+++ KISKA Systems/KISKA Parameter Menu/Headers/params menu.hpp
@@ -216,7 +216,7 @@ class paramsMenu
         	y = POS_Y(-13);
         	w = POS_W(9);
         	h = POS_H(1);
-            tooltip = "This will brodcast your changes over the network. Be aware that depending on the amount of changes, it can significantly and temporarily impact perfromance";
+            tooltip = "This will brodcast your changes over the network to all connected machines and add the updates to the JIP queue.";
         };
         class paramsMenu_messageBox : RscListbox
         {
